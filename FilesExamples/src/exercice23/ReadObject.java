1c756d4084e1e8d387cd9046121f9ce1eaebdb00
alumno <alumno@10.203.28.138>
2026-04-14 12:02:07 +0200

    Exercice23 almost finished in class

--- FilesExamples/src/exercice23/ReadObject.java
+++ FilesExamples/src/exercice23/ReadObject.java
@@ -11,5 +11,31 @@ public class ReadObject {
 		
 		final String OUT_PATH= "C:\\ProgFiles\\employees.ftw";
 		
+		
+		 try {
+			ObjectInputStream inputEmployee = new ObjectInputStream(new FileInputStream(OUT_PATH));
+		
+			System.out.println("Employees List:\n");
+			
+			
+			while(true) {
+				Employees emp;
+				try {
+					emp = (Employees) inputEmployee.readObject();
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println(emp);
+			}
+		 
+		 } catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+
 	}
 }
--- FilesExamples/src/exercice23/WriteObject.java
+++ FilesExamples/src/exercice23/WriteObject.java
@@ -1,10 +1,14 @@
 package exercice23;
 
+import java.io.EOFException;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class WriteObject {
 
@@ -12,24 +16,72 @@ public class WriteObject {
 		
 		final String OUT_PATH= "C:\\ProgFiles\\employees.ftw";
 		Scanner keyboard = new Scanner (System.in);
+		ArrayList<Employe>employees = new ArrayList<>();
+		
+		writeEmployee(OUT_PATH, keyboard);
+		
+		readEmployee(OUT_PATH);
+		
+		
+		keyboard.close();
+	}
+
+	private static void readEmployee(final String OUT_PATH,Arraylist<Employe> employees) {
 		
 		try {
-			ObjectOutputStream writeObject = new ObjectOutputStream(new FileOutputStream(OUT_PATH));
-			Employees emp1 = new Employees();
-			Employees emp2 = new Employees("Valentino", 1300f, 23);
-			Employees emp3 = new Employees("Diana", 1400f, 24);
+			ObjectInputStream inputEmploye = new ObjectInputStream(new FileInputStream(OUT_PATH));
+			System.out.println("Employees List:\n");
+		
+			while(true) {
+				
+					Employees emp = (Employees) inputEmploye.readObject();
+					System.out.println(emp);
+					
+			}
 			
-			writeObject.writeObject(emp1);
-			writeObject.writeObject(emp2);
-			writeObject.writeObject(emp3);
+		} catch (EOFException e) {
+			System.out.println("\nEnd of file reached.");
 			
-		} catch (FileNotFoundException e) {
+		} catch (IOException | ClassNotFoundException e) {
 			
 			e.printStackTrace();
+			
+		}
+	}
+
+	private static void writeEmployee(final String OUT_PATH, Scanner keyboard) {
+		try {
+			ObjectOutputStream employeFile = new ObjectOutputStream(new FileOutputStream(OUT_PATH));
+			System.out.println("How many employees?: ");
+			int numEmp = keyboard.nextInt();
+			keyboard.nextLine();
+			
+			for(int i = 0; i<numEmp; i++) {
+				
+				System.out.println("Employee "+(i+1));
+				
+				System.out.println("Name: ");
+				String name = keyboard.nextLine();
+				
+				System.out.println("Salary: ");
+				float salary = keyboard.nextFloat();
+				
+				System.out.println("Age: ");
+				int age = keyboard.nextInt();
+				keyboard.nextLine();
+				
+				Employees emp = new Employees(name, salary, age);
+				employeFile.writeObject(emp);
+				
+			}
+			
+			employeFile.close();
+			System.out.println("Employee saved correctly");
+			
+			
 		} catch (IOException e) {
 			
 			e.printStackTrace();
 		}
-	
 	}
 }
